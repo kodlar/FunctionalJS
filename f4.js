13a14d741140e63551b3bda10779653601ad004d
kodlar <ogankeskiner@gmail.com>
2016-06-20 01:07:15 +0300

    f4.js dosyasi eklendi

---
+++ f4.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+//output like that
+/*
+var data = {
+  'ali' : [ { name :'blender', price : '50', quantity : '2'},
+{ name :'supurge', price : '40', quantity : '1'},
+{ name :'bardak', price : '34', quantity : '5'}],
+  'burak' : [{ name :'terlik', price : '23', quantity : '1'},
+{ name :'cdlik', price : '50', quantity : '1'},
+{ name :'resim', price : '50', quantity : '2'}]
+}
+
+console.log(data)
+*/
