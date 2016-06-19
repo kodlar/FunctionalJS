d0dbfa5778fd63daedbe190abfbae43af499d5ba
kodlar <ogankeskiner@gmail.com>
2016-06-20 01:40:06 +0300

    reduce fonksiyonu eklendi ve datada aralıklar tab olacak şekilde düzenlendi

--- f4.js
+++ f4.js
@@ -4,7 +4,7 @@
 // yeni yazım import diyebilmek için
 // "babel": "^6.5.2",
 //  "babel-cli": "^6.10.1",
-//  "babel-preset-es2015": "^6.9.0" paketlerini kurdum
+//  "babel-preset-es2015": "^6.9.0" paketlerini kurdum, .babelrc dosyasını ekledim
 // sonra babel f4.js | node çalıştırdım
 // utf8 character encoding koymadığın zaman buffer bir byte değeri dönüyour
 // utf8 ekleyince textin kendisi dönüyour
@@ -15,4 +15,7 @@ var output = fs.readFileSync('f4.txt','utf8')
             .trim()
             .split('\n')
             .map(line => line.split('\t'))
+            .reduce((customer, line) => {
+              return customer
+            }, {})
 console.log('output', output)
